c6fbf1d59598a5b76d9b7d4c9e76e764f17bf5bb
Judah Wilson <judah.d.wilson@gmail.com>
2026-01-10 22:00:39 -0600

    WIP

--- statbucket/scraping/utilities.py
+++ statbucket/scraping/utilities.py
@@ -1,1 +1,6 @@
-def _parse_table()
+import pandas as pd
+
+def _parse_table_BR(contents: str) -> pd.DataFrame:
+    """Get a DataFrame from an HTML table but preserve the a tags."""
+    contents = contents.replace('<a ', '***a ').replace('</a>', '***/a>')
+    df = pd.read_html(contents)[0]
