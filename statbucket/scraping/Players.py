c04b7c7e58e6a50334844fce8c1cdbb9784f4dd3
Judah Wilson <judah.d.wilson@gmail.com>
2026-01-10 22:30:39 -0600

    WIP

--- statbucket/scraping/Players.py
+++ statbucket/scraping/Players.py
@@ -17,18 +17,18 @@ class Players(BaseScraper):
             range_end=range_end,
             override_html_cache=override_html_cache,
         )
-        self.letters = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+        self._letters = [chr(i) for i in range(ord("a"), ord("z") + 1)]
         """Letters a-z except x"""
-        self.letters.pop(self.letters.index("x"))  # No players with last names starting with X
+        self._letters.pop(self._letters.index("x"))  # No players with last names starting with X
 
     def _scrape(self, sid: str | None = None):
         first_letter = "a" if sid is None else sid[0].lower()
-        start_index = self.letters.index(first_letter)
-        for letter in self.letters[start_index:]:
+        start_index = self._letters.index(first_letter)
+        for letter in self._letters[start_index:]:
             players_soup = self._html(f"players/{letter}/", "table#players")
             players_df = parse_table_BR(players_soup)
             breakpoint()
-            
+
 
     
     def _persist(self):
