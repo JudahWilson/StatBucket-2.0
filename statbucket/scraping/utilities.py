3c0925f7bcda03b7e0e1bf862a9c9029ab528c2b
Judah Wilson <judah.d.wilson@gmail.com>
2026-01-12 17:28:50 -0600

    WIP

--- statbucket/scraping/utilities.py
+++ statbucket/scraping/utilities.py
@@ -2,11 +2,8 @@ import pandas as pd
 from bs4 import Tag, BeautifulSoup
 
 
-def parse_table_BR(contents: str | Tag) -> pd.DataFrame:
-    """Get a DataFrame from an HTML table but preserve the a tags."""
-
-    contents_str = str(contents)
-    soup = BeautifulSoup(contents_str, "html.parser")
+def encode_links(soup: BeautifulSoup) -> BeautifulSoup:
+    """Encode all <a> tags in the soup to preserve text and href."""
 
     for link in soup.find_all("a"):
         href = link.get("href", "")
@@ -14,6 +11,14 @@ def parse_table_BR(contents: str | Tag) -> pd.DataFrame:
         # Replace link with a formatted string that preserves both text and href
         link.replace_with(f"LINK[{text}|{href}]")
 
+    return soup
+
+def parse_table_BR(contents: str | Tag) -> pd.DataFrame:
+    """Get a DataFrame from an HTML table but preserve the a tags."""
+
+    contents_str = str(contents)
+    soup = BeautifulSoup(contents_str, "html.parser")
+    soup = encode_links(soup)
     df = pd.read_html(str(soup))[0]
 
     return df
